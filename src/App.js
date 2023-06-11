40d04038f1ef7c2b1fc01bd072912af1ad206e76
K.D Rushin Sandeepana <rushinsandeepane11@gmail.com>
2023-06-11 13:38:48 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import TopBar from "./Components/TopBar";
+import QuestionsPage from "./Pages/QuestionsPage";
+import EssayPage from './Pages/EssayPage';
+import McqPage from './Pages/McqPage';
+import './Components/TopBar.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  return(
+    <div>
+      <TopBar />
+      <Routes>
+        <Route path="/" element={<QuestionsPage />} />
+        <Route path="essayPage" element={<EssayPage />} />
+        <Route path="mcqPage" element={<McqPage />} />
+      </Routes>
     </div>
   );
 }
